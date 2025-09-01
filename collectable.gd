b48077f1f1aaadf0e3ddad5733f4c7666a2f6551
Aedyn Ladd <awiladd@gmail.com>
2025-09-01 13:09:14 -0400

    exported

--- collectable.gd
+++ collectable.gd
@@ -1,6 +1,7 @@
 extends Area3D
 
+@onready var Manager = %GameManager
 
 func _on_body_entered(body: Node3D) -> void:
-	print("hello") # Replace with function body.
 	queue_free()
+	Manager.add_point(1)
--- game_manager.gd
+++ game_manager.gd
@@ -1,6 +1,10 @@
 extends Node3D
 
-
+var POINTS = 0
+func add_point(val: int):
+	POINTS += val
+	print(POINTS)
+	
 func _ready():
 	print("done")
 	
